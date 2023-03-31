e3ea95a73db36e851ecbc026f46d35212d7c3b79
Jaiaid <jaiaidmobin@gmail.com>
2023-03-31 04:21:36 -0400

    possible solution of query 2

--- phasetwo/psql_queries.sql
+++ phasetwo/psql_queries.sql
@@ -1,3 +1,23 @@
+/* How many people are picked up at an NYC airport for New Years Eve */
+SELECT 
+    AVG(Trip.pickupLatitude) AS averageApproximatePickupLatitude,
+    AVG(Trip.pickupLongitude) AS averageApproximatePickupLongitude,
+    SUM(PassengerCount) AS count
+FROM
+    Trip
+    INNER JOIN Vendortrips ON Vendortrips.tripid = Trip.tripid
+WHERE
+    TO_CHAR(pickupdatetime, 'dd-mm') = '01-01'
+    AND
+    (
+        earth_distance(ll_to_earth(pickupLatitude,pickupLongitude),ll_to_earth(40.64131, -73.77892)) < 1000
+        OR
+        earth_distance(ll_to_earth(pickupLatitude,pickupLongitude),ll_to_earth(40.68953, -74.17456)) < 1000
+        OR
+        earth_distance(ll_to_earth(pickupLatitude,pickupLongitude),ll_to_earth(40.77692, -73.87353)) < 1000
+    )
+GROUP BY ROUND(pickupLatitude,2), ROUND(pickupLongitude, 2)
+ORDER BY count DESC;
 /* Who is the most popular taxi vendor in NYC based off of their maximum profits */
 
 SELECT 
